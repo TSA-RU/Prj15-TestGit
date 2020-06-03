7dc437c65a00c33d16633ecc7ca7a3f6901e8495
Sergey Tolnykin <tsa@tsa.ru>
2020-06-03 09:29:22 +0300

    Initial Commit

---
+++ Prj15 TestGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prj15 TestGit
+//
+//  Created by Sergey Tolnykin on 03.06.2020.
+//  Copyright © 2020 Sergey Tolnykin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
